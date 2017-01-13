8e5bdec9096198b80a930abbf4e9e01e43d5783c
quentin <quentin.etu@outlook.com>
2017-01-13 17:51:54 +0100

    add_interface

--- Interfaces/InterfaceCo2Sensor.java
+++ Interfaces/InterfaceCo2Sensor.java
@@ -2,7 +2,7 @@ public interface InterfaceCo2Sensor {
 
     /**
     * @return a list of data
-    * @param start the beginning of the period
+    * @param start  the beginning of the period
     * @param end the end of the period
     **/
     public ArrayList<Float> getCo2OnPeriod(Date start, Date end);
