f9e74171674e4513b78e769b5c8261d7c8fd6bf4
mathieu <mathieu.colne@hotmail.fr>
2017-01-18 16:18:39 +0100

    InterfaceSensors

--- nsoc/src/main/java/sensor/sensorInterface/InterfaceSensor.java
+++ nsoc/src/main/java/sensor/sensorInterface/InterfaceSensor.java
@@ -6,19 +6,19 @@ import java.util.Date;
 /**
  * Created by mathieu on 16/01/2017.
  */
-public interface InterfaceConsumptionSensor {
+public interface InterfaceSensors {
     /**
      * @param start the beginning of the period
      * @param end the end of the period
      * @return a list of data
      **/
-    ArrayList<Float> getConsumptiomOnPeriod(Date start, Date end);
+    ArrayList<Float> getValuesOnPeriod(Date start, Date end);
 
 
     /**
      * @return the last data
      **/
-    float getLastConsumption();
+    float getLastValue();
 
 
     /**
