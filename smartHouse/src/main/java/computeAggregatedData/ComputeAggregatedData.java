1751c387dc80b3820f3c041e367fad69ee2e8aff
mathieu <mathieu.colne@hotmail.fr>
2017-01-20 17:44:20 +0100

    Add rules

--- smartHouse/src/main/java/computeAggregatedData/ComputeAggregatedData.java
+++ smartHouse/src/main/java/computeAggregatedData/ComputeAggregatedData.java
@@ -41,22 +41,34 @@ public class ComputeAggregatedData implements InterfaceComputeAggregatedData{
 
     public void createCO2Indicator(float currentCO2Value) {
         //Double co2Setpoint = db.co2Setpoint();
+        //Double indicator = co2Setpoint - currentCO2Value;
+        //mettre en base
     }
     public void createTemperatureIndicator(float currentTemperature) {
         //Double temperatureSetpoint = db.temperatureSetpoint();
+        //Double indicator = temperatureSetpoint - currentTemperature;
+        //mettre en base
     }
     public void createHumidityIndicator(float currentHumidity) {
         //Double humiditySetpoint = db.humiditySetpoint();
+        //Double indicator = humiditySetpoint - currentHumidity;
+        //mettre en base
     }
     public void createElectricityConsumptionIndicator(float currentElectricityConsumption) {
-        // Double consumptionSetpoint = db.consumptionSetpoint();
+        //Double consumptionSetpoint = db.consumptionSetpoint();
+        //Double indicator = consumptionSetpoint - currentElectricityConsumption;
+        //mettre en base
     }
     public void createElectricityProductionIndicator(float currentElectricityProduction) {
         //Double productionSetpoint = db.productionSetpoint();
+        //Double indicator = productionSetpoint - currentElectricityProduction;
+        //mettre en base
     }
     public void createElectricityRatioIndicator() {
         Double currentConsumptionValue = consumption.getLastValue();
         Double currentProductionValue = production.getLastValue();
-
+        Double indicator = currentProductionValue- currentConsumptionValue;
+        //traiter l'indicateur
+        //mettre l'indicateur en base
     }
 }
