39b0021a3ec69e6810c455e5f2553b044fcb5eb7
clement <clement.cheron@wanadoo.fr>
2017-01-20 14:09:35 +0100

    merging

--- smartHouse/script.sh
+++ smartHouse/script.sh
@@ -1,3 +1,4 @@
 mvn clean
 mvn install:install-file -Dfile=libraries/bacnet.jar -DgroupId=nsoc -Dversion=1.0 -DartifactId=bacnet -Dpackaging=jar
+mvn package
 mvn install
--- smartHouse/src/main/java/bacnet/BacNetToJava.java
+++ smartHouse/src/main/java/bacnet/BacNetToJava.java
@@ -74,12 +74,12 @@ public  class  BacNetToJava implements InterfaceReadBacnet {
         return cs;
     }
 
-    private static void disconnection(){
+   /* private static void disconnection(){
         localDevice.terminate();
-    }
+    }*/
 
 
-    private static void connection(){
+   /* private static void connection(){
         // cr�ation du listener
         localDevice.getEventHandler().addListener(new Listener());
         localDevice.terminate();
@@ -95,12 +95,12 @@ public  class  BacNetToJava implements InterfaceReadBacnet {
             // TODO Auto-generated catch block
             System.out.println("Probl�me de connexion avec la maquette !");
         }
-    }
+    }*/
 
-    static class Listener extends DeviceEventAdapter {
+   /* static class Listener extends DeviceEventAdapter {
         @Override
         public void iAmReceived(RemoteDevice d) {
             System.out.println("I am received" + d);
         }
-    }
+    }*/
 }
