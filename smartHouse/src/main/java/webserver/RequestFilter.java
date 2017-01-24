f1755f1023f2b5cb99dd59edf91be3c6da0bb8a7
David <david.chaille@carbip.com>
2017-01-24 17:25:46 +0100

    verify client's session with userId and token in database

--- smartHouse/src/main/java/webserver/RequestFilter.java
+++ smartHouse/src/main/java/webserver/RequestFilter.java
@@ -1,10 +1,14 @@
 package webserver;
 
+import database.ReadInDatabase;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class RequestFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
@@ -12,10 +16,24 @@ public class RequestFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpSession session = request.getSession(false);
         if(session!=null){
-
-            String name=(String)session.getAttribute("name");
-            chain.doFilter(servletRequest, servletResponse);
-            System.out.println("Hello, "+name+" Welcome to Profile");
+            String userId = (String)session.getAttribute("userId");
+            String userToken= (String)session.getAttribute("userToken");
+            HashMap userSession = null;
+            try {
+                userSession = ReadInDatabase.getUserSession(userId).get(0);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (userSession.get("token").equals(userToken) && userSession.get("userid").equals(userId)) {
+                String name=(String)session.getAttribute("name");
+                chain.doFilter(servletRequest, servletResponse);
+                System.out.println("Hello, "+name+" Welcome to Profile");
+            }
+            else{
+                System.out.println("Please login first");
+                HttpServletResponse response = (HttpServletResponse) servletResponse;
+                response.sendRedirect("/");
+            }
 
         }
         else{
