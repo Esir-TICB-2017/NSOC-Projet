593e3fcea3828d908be3cc65109f9ea36c7780c2
Louison Gitzinger <gitzingerlouison@gmail.com>
2017-01-24 16:36:03 +0100

    au calme l'auth

--- smartHouse/src/main/webapp/resources/app.js
+++ smartHouse/src/main/webapp/resources/app.js
@@ -42,11 +42,12 @@
         console.log(JSON.parse(data));
       }
 
-        function signOut() {
+     function signOut() {
             var auth2 = gapi.auth2.getAuthInstance();
             auth2.signOut().then(function () {
+              console.log('User signed out.');
               var xmlHttp = new XMLHttpRequest();
-                     xmlHttp.open("GET", "api/logout", true); // true for asynchronous
+                     xmlHttp.open("GET", "/logout", true); // true for asynchronous
                       xmlHttp.send(null);
 
             });
