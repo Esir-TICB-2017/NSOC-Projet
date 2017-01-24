bca840381f517e769b64b5b68f6aa09cce1dfcd8
Louison Gitzinger <gitzingerlouison@gmail.com>
2017-01-24 22:46:26 +0100

    super

--- smartHouse/src/main/webapp/static/login.js
+++ smartHouse/src/main/webapp/static/login.js
@@ -12,14 +12,3 @@ function onSignIn(googleUser) {
     };
     xhr.send('idtoken=' + id_token);
 };
-
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("GET", "/logout", true); // true for asynchronous
-        xmlHttp.send(null);
-
-    });
-}
