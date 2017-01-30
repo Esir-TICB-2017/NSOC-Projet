98e2e3f3220796b99ca5481639654b47c7eef8b1
David <david.chaille@carbip.com>
2017-01-30 16:29:11 +0100

    buttons active

--- smartHouse/src/main/webapp/static/js/controllers/general.js
+++ smartHouse/src/main/webapp/static/js/controllers/general.js
@@ -4,8 +4,9 @@
 angular.module('nsoc')
     .controller('generalController', ($scope) => {
 			$scope.selectors = ['Monthly', 'Weekly', 'Daily'];
-        $scope.getData = function () {
-            console.log('ici');
+			$scope.actualSelector = $scope.selectors[0];
+        $scope.getData = function (selector) {
+					$scope.actualSelector = selector;
         }
 
         const now = parseInt(new Date().getTime() / 1000);
