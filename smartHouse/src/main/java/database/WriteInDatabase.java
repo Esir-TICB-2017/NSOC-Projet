cbd8f16c1012bc99f37a4bd98d09804289f51e7c
clement <clement.cheron@wanadoo.fr>
2017-02-13 19:11:41 +0100

    round indicators

--- smartHouse/src/main/java/database/WriteInDatabase.java
+++ smartHouse/src/main/java/database/WriteInDatabase.java
@@ -15,6 +15,8 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import static jdk.nashorn.internal.objects.NativeMath.round;
+
 /**
  * Created by loulou on 21/01/2017.
  */
@@ -43,6 +45,8 @@ public class WriteInDatabase extends Database implements InterfaceWriteDatabase 
 	}
 
 	public static void writeIndicatorValue(Indicator indicator, Double value) {
+		double val  = Math.round(value*100);
+		Double realVal = val/100;
 		Connection connection = ConnectionManager.getConnection();
 		Timestamp currentDate = Utils.getCurrentTimeStamp();
 		String sql = "INSERT INTO indicators "
@@ -50,7 +54,7 @@ public class WriteInDatabase extends Database implements InterfaceWriteDatabase 
 				+ "(?, ?, ?)";
 		try {
 			PreparedStatement preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setDouble(1, value);
+			preparedStatement.setDouble(1, realVal);
 			preparedStatement.setTimestamp(2, currentDate);
 			preparedStatement.setInt(3, indicator.getId());
 			preparedStatement.executeUpdate();
@@ -58,7 +62,7 @@ public class WriteInDatabase extends Database implements InterfaceWriteDatabase 
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		DatabaseEventsHandler.broadcastValue(new DataRecord(value, currentDate, "indicator", indicator.getType()));
+		DatabaseEventsHandler.broadcastValue(new DataRecord(realVal, currentDate, "indicator", indicator.getType()));
 	}
 
 	public static void setSensorStatus(Sensor sensor, Boolean status) {
