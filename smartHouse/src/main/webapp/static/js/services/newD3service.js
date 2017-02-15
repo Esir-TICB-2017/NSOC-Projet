dcb9ccd78b638e58d3a12cc2fd142ccd7805f07d
Louison <gitzingerlouison@gmail.com>
2017-02-15 13:35:08 +0100

    tooltip !

--- smartHouse/src/main/webapp/static/js/services/newD3service.js
+++ smartHouse/src/main/webapp/static/js/services/newD3service.js
@@ -7,16 +7,91 @@
 angular.module('nsoc').factory('newD3Service', () => {
     const constants = {};
     const currentData = {};
-    function tooltip() {
-        console.log(d3.event.target.className.baseVal)
-        const elementClassName = d3.event.target.className.baseVal;
-        if(elementClassName === 'line' || elementClassName === 'area') {
-            console.log(d3.event)
+    const tooltip = {
+        xRect: -100,
+        yRect: -100,
+        rectWidth: 60,
+        rectHeight: 40,
+    };
+
+    function mousemoved() {
+        var pathLength = d3.select('.line').node().getTotalLength();
+        var x = d3.event.pageX;
+        var beginning = x, end = pathLength, target;
+        while (true) {
+            target = Math.floor((beginning + end) / 2);
+            pos = d3.select('.line').node().getPointAtLength(target);
+            if ((target === end || target === beginning) && pos.x !== x) {
+                break;
+            }
+            if (pos.x > x)      end = target;
+            else if (pos.x < x) beginning = target;
+            else                break; //position found
+        }
+
+        const tooltipGroup = d3.select('g.tooltip')
+            .attr('opacity', 1);
+        tooltipGroup.select('circle').attr("cx", x).attr("cy", pos.y);
+        let yTooltipRect = pos.y - 50;
+        if (yTooltipRect < 0) {
+            yTooltipRect = pos.y + 20;
+        }
+        tooltipGroup.select('rect').attr('x', tooltip.xRect = x - tooltip.rectWidth / 2).attr('y', tooltip.yRect = yTooltipRect)
+        tooltipGroup.select('text')
+            .attr('x', tooltip.xRect + tooltip.rectWidth / 2)
+            .attr('y', tooltip.yRect + tooltip.rectHeight / 2)
+            .text(constants.yScale.invert(pos.y).toFixed(1));
+    }
+
+    function closestPoint(pathNode, point) {
+        console.log(pathNode.getPointAtLength())
+        var pathLength = pathNode.getTotalLength(),
+            precision = 8,
+            best,
+            bestLength,
+            bestDistance = Infinity;
+
+        // linear scan for coarse approximation
+        for (var scan, scanLength = 0, scanDistance; scanLength <= pathLength; scanLength += precision) {
+            if ((scanDistance = distance2(scan = pathNode.getPointAtLength(scanLength))) < bestDistance) {
+                best = scan, bestLength = scanLength, bestDistance = scanDistance;
+            }
+        }
+
+        // binary search for precise estimate
+        precision /= 2;
+        while (precision > 0.5) {
+            var before,
+                after,
+                beforeLength,
+                afterLength,
+                beforeDistance,
+                afterDistance;
+            if ((beforeLength = bestLength - precision) >= 0 && (beforeDistance = distance2(before = pathNode.getPointAtLength(beforeLength))) < bestDistance) {
+                best = before, bestLength = beforeLength, bestDistance = beforeDistance;
+            } else if ((afterLength = bestLength + precision) <= pathLength && (afterDistance = distance2(after = pathNode.getPointAtLength(afterLength))) < bestDistance) {
+                best = after, bestLength = afterLength, bestDistance = afterDistance;
+            } else {
+                precision /= 2;
+            }
+        }
+
+        best = [best.x, best.y];
+        best.distance = Math.sqrt(bestDistance);
+        return best;
+
+        function distance2(p) {
+            var dx = p.x - point[0],
+                dy = p.y - point[1];
+            return dx * dx + dy * dy;
         }
     }
+
     return {
         init: () => {
-            const svg = d3.select(`svg#homeChart`);
+
+            const svg = d3.select(`svg#homeChart`).on("mousemove", mousemoved);
+            ;
             constants.width = parseInt(svg.style('width'), 0);
             constants.height = parseInt(svg.style('height'), 0);
             constants.widthMargin = 15;
@@ -48,26 +123,19 @@ angular.module('nsoc').factory('newD3Service', () => {
             const line = svg.append('path')
                 .attr("class", "line")
                 .attr("d", constants.lineGen(currentData.data))
-                .on('mouseover', (d) => {
-                    tooltip();
-                    // console.log(d3.event.target.className.baseVal)
-                    //  if(d3.event.target === 'path' )
-                });
-            /*
-            var totalLength = line.node().getTotalLength();
 
-            line
-                .attr("stroke-dasharray", totalLength + " " + totalLength)
-                .attr("stroke-dashoffset", totalLength)
-                .transition()
-                .duration(2000)
-                .ease(d3.easeLinear)
-                .attr("stroke-dashoffset", 0);
-            */
+            /*
+             var totalLength = line.node().getTotalLength();
+
+             line
+             .attr("stroke-dasharray", totalLength + " " + totalLength)
+             .attr("stroke-dashoffset", totalLength)
+             .transition()
+             .duration(2000)
+             .ease(d3.easeLinear)
+             .attr("stroke-dashoffset", 0);
+             */
             svg.append("path")
-                .on('mouseover', (d) => {
-                    tooltip();
-                })
                 .attr("class", "area")
                 .style('opacity', 0)
                 .transition()
@@ -75,16 +143,6 @@ angular.module('nsoc').factory('newD3Service', () => {
                 .ease(d3.easeLinear)
                 .style('opacity', 1)
                 .attr("d", constants.areaGen(currentData.data));
-console.log(currentData.data)
-            svg.append("circle")
-                .data(currentData.data)
-                .attr('fill', 'red')
-                .attr("r", 5)
-                .attr("cx", function(d) {
-                    console.log(d)
-                    return constants.xScale(d.date); })
-                .attr("cy", function(d) { return constants.yScale(d.data); })
-
 
 
             svg.append("g")
@@ -93,13 +151,37 @@ console.log(currentData.data)
                 .call(constants.xAxis);
             svg.append("g")
                 .attr('class', 'yAxis')
-                .attr("transform", "translate(60, -10)")
+                .attr("transform", "translate(60, 0)")
                 .call(constants.yAxis);
+
+            const tooltipGroup = svg.append('g')
+                .attr('class', 'tooltip');
+
+
+            tooltipGroup.append("circle")
+                .attr("cx", -10)
+                .attr("cy", -10)
+                .attr("r", 3.5)
+                .attr('fill', 'white');
+
+            tooltipGroup.append('rect')
+                .attr('x', tooltip.xRect)
+                .attr('y', tooltip.yRect)
+                .attr('width', tooltip.rectWidth)
+                .attr('height', tooltip.rectHeight)
+                .attr('fill', 'white')
+                .attr('rx', 4)
+                .attr('ry', 4);
+
+            tooltipGroup.append('text')
+                .attr('dy', '.35em')
+                .attr('fill', 'red')
+                .style('text-anchor', 'middle')
+                .style('font-size', 15)
         },
         update: () => {
 
-
-
+            d3.select('g.tooltip').attr('opacity', 0);
             constants.xScale
                 .domain([new Date(currentData.data[0].date), new Date(currentData.data[currentData.data.length - 1].date)]);
             constants.yScale
